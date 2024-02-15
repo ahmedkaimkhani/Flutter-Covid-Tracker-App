b4b3bc9e435e87e3c67d1efc9a04ba0377036079
ahmedkaimkhani <ak9302359@gmail.com>
2024-02-16 02:52:41 +0500

    Covid Tracker App

--- lib/Services/Utilities/app_url.dart
+++ lib/Services/Utilities/app_url.dart
@@ -3,5 +3,5 @@ class AppUrl {
       'https://webhook.site/4700d694-5556-4d09-af6f-8574c7981108';
 
   static const countriesListApi =
-      'https://webhook.site/5752f0c5-9632-40ae-98d8-530778d40172';
+      'https://webhook.site/2dcfc083-0c8e-47dc-b232-ea9f9f73c855';
 }
--- lib/Views/countries_list_view.dart
+++ lib/Views/countries_list_view.dart
@@ -34,64 +34,67 @@ class _CountriesListViewState extends State<CountriesListView> {
                         borderRadius: BorderRadius.circular(50))),
               ),
             ),
-            FutureBuilder(
-                future: stateServices.countriesListApi(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<List<dynamic>> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Shimmer.fromColors(
-                      baseColor: Colors.grey.shade800,
-                      highlightColor: Colors.grey.shade100,
-                      child: ListView.builder(
-                          itemCount: 4,
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              title: Container(
-                                height: 10,
-                                width: 90,
-                                color: Colors.white,
-                              ),
-                              subtitle: Container(
-                                height: 10,
-                                width: 90,
-                                color: Colors.white,
-                              ),
-                              leading: Container(
-                                height: 50,
-                                width: 50,
-                                color: Colors.white,
-                              ),
-                            );
-                          }),
-                    );
-                  } else if (snapshot.hasError) {
-                    print("Error: ${snapshot.error}");
-                    return Center(
-                      child: Text(snapshot.error.toString()),
-                    );
-                  } else {
-                    print("${snapshot.data}");
-                    return ListView.builder(
-                        itemCount: snapshot.data!.length,
-                        itemBuilder: (context, index) {
-                          return Column(
-                            children: [
-                              ListTile(
-                                title: Text(snapshot.data![index]['country']),
-                                subtitle: Text(snapshot.data![index]['infected']
-                                    .toString()),
-                                leading: Image(
+            Expanded(
+              child: FutureBuilder(
+                  future: stateServices.countriesListApi(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<dynamic>> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Shimmer.fromColors(
+                        baseColor: Colors.grey.shade800,
+                        highlightColor: Colors.grey.shade100,
+                        child: ListView.builder(
+                            itemCount: 4,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                title: Container(
+                                  height: 10,
+                                  width: 90,
+                                  color: Colors.white,
+                                ),
+                                subtitle: Container(
+                                  height: 10,
+                                  width: 90,
+                                  color: Colors.white,
+                                ),
+                                leading: Container(
                                   height: 50,
                                   width: 50,
-                                  image: NetworkImage(
-                                      snapshot.data![index]['flag'].toString()),
+                                  color: Colors.white,
                                 ),
-                              ),
-                            ],
-                          );
-                        });
-                  }
-                }),
+                              );
+                            }),
+                      );
+                    } else if (snapshot.hasError) {
+                      print("Error: ${snapshot.error}");
+                      return Center(
+                        child: Text(snapshot.error.toString()),
+                      );
+                    } else {
+                      return ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            return Column(
+                              children: [
+                                ListTile(
+                                  title: Text(snapshot.data![index]['country']),
+                                  subtitle: Text(snapshot.data![index]
+                                          ['infected']
+                                      .toString()),
+                                  leading: Image(
+                                    height: 50,
+                                    width: 50,
+                                    image: NetworkImage(snapshot.data![index]
+                                            ['flag']
+                                        .toString()),
+                                  ),
+                                ),
+                              ],
+                            );
+                          });
+                    }
+                  }),
+            ),
           ],
         ),
       ),
