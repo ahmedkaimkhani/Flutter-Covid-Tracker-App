af722d2ce378d15be02b378f0878116c9461022b
ahmedkaimkhani <ak9302359@gmail.com>
2024-02-17 02:15:00 +0500

    Covid Tracker App

--- lib/Services/Utilities/app_url.dart
+++ lib/Services/Utilities/app_url.dart
@@ -1,6 +1,6 @@
 class AppUrl {
   static const worldStatesApi =
-      'https://webhook.site/4700d694-5556-4d09-af6f-8574c7981108';
+      'https://webhook.site/5a6bf385-f69e-4365-8ce0-5b2520f26fce';
 
   static const countriesListApi =
       'https://webhook.site/2dcfc083-0c8e-47dc-b232-ea9f9f73c855';
--- lib/Views/world_states_view.dart
+++ lib/Views/world_states_view.dart
@@ -2,6 +2,7 @@ import 'package:covid_tracker/Models/world_states_model.dart';
 import 'package:covid_tracker/Services/state_services.dart';
 import 'package:covid_tracker/Views/countries_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:pie_chart/pie_chart.dart';
 
@@ -34,122 +35,130 @@ class _WorldStatesViewState extends State<WorldStatesView>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.01,
-            ),
-            FutureBuilder(
-                future: stateServices.getWorldStatesRecord(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<WorldStatesModel> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Expanded(
-                      flex: 1,
-                      child: SpinKitFadingCircle(
-                        color: Colors.white,
-                        controller: _animationController,
-                        size: 50,
-                      ),
-                    );
-                  } else if (snapshot.hasError) {
-                    print(snapshot.error);
-                    return Center(
-                      child: Text(snapshot.error.toString()),
-                    );
-                  } else if (snapshot.data == null) {
-                    return const Center(
-                      child: Text('Data is null'),
-                    );
-                  } else {
-                    return Expanded(
-                      child: Column(
-                        children: [
-                          PieChart(
-                            dataMap: {
-                              'Total':
-                                  double.parse(snapshot.data!.cases.toString()),
-                              'Recoverd': double.parse(
-                                  snapshot.data!.recovered.toString()),
-                              'Deaths': double.parse(
-                                  snapshot.data!.deaths.toString()),
-                            },
-                            animationDuration:
-                                const Duration(milliseconds: 1200),
-                            chartType: ChartType.ring,
-                            colorList: colorList,
-                            chartRadius:
-                                MediaQuery.of(context).size.width / 3.2,
-                            legendOptions: const LegendOptions(
-                                legendPosition: LegendPosition.left),
-                            chartValuesOptions: const ChartValuesOptions(
-                                showChartValuesInPercentage: true),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(
-                                vertical:
-                                    MediaQuery.of(context).size.height * 0.06),
-                            child: Card(
-                              child: Column(
-                                children: [
-                                  ReusableRow(
-                                      title: 'Total',
-                                      value: snapshot.data!.cases.toString()),
-                                  ReusableRow(
-                                      title: 'Recovered',
-                                      value:
-                                          snapshot.data!.recovered.toString()),
-                                  ReusableRow(
-                                      title: 'Deaths',
-                                      value: snapshot.data!.deaths.toString()),
-                                  ReusableRow(
-                                      title: 'Active',
-                                      value: snapshot.data!.active.toString()),
-                                  ReusableRow(
-                                      title: 'Critical',
-                                      value:
-                                          snapshot.data!.critical.toString()),
-                                  ReusableRow(
-                                      title: 'Affected Countries',
-                                      value: snapshot.data!.affectedCountries
-                                          .toString()),
-                                  ReusableRow(
-                                      title: 'Tests',
-                                      value: snapshot.data!.tests.toString()),
-                                ],
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) {
+        SystemNavigator.pop();
+      },
+      child: Scaffold(
+        body: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.01,
+              ),
+              FutureBuilder(
+                  future: stateServices.getWorldStatesRecord(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<WorldStatesModel> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Expanded(
+                        flex: 1,
+                        child: SpinKitFadingCircle(
+                          color: Colors.white,
+                          controller: _animationController,
+                          size: 50,
+                        ),
+                      );
+                    } else if (snapshot.hasError) {
+                      print(snapshot.error);
+                      return Center(
+                        child: Text(snapshot.error.toString()),
+                      );
+                    } else if (snapshot.data == null) {
+                      return const Center(
+                        child: Text('Data is null'),
+                      );
+                    } else {
+                      return Expanded(
+                        child: Column(
+                          children: [
+                            PieChart(
+                              dataMap: {
+                                'Total': double.parse(
+                                    snapshot.data!.cases.toString()),
+                                'Recoverd': double.parse(
+                                    snapshot.data!.recovered.toString()),
+                                'Deaths': double.parse(
+                                    snapshot.data!.deaths.toString()),
+                              },
+                              animationDuration:
+                                  const Duration(milliseconds: 1200),
+                              chartType: ChartType.ring,
+                              colorList: colorList,
+                              chartRadius:
+                                  MediaQuery.of(context).size.width / 3.2,
+                              legendOptions: const LegendOptions(
+                                  legendPosition: LegendPosition.left),
+                              chartValuesOptions: const ChartValuesOptions(
+                                  showChartValuesInPercentage: true),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: MediaQuery.of(context).size.height *
+                                      0.06),
+                              child: Card(
+                                child: Column(
+                                  children: [
+                                    ReusableRow(
+                                        title: 'Total',
+                                        value: snapshot.data!.cases.toString()),
+                                    ReusableRow(
+                                        title: 'Recovered',
+                                        value: snapshot.data!.recovered
+                                            .toString()),
+                                    ReusableRow(
+                                        title: 'Deaths',
+                                        value:
+                                            snapshot.data!.deaths.toString()),
+                                    ReusableRow(
+                                        title: 'Active',
+                                        value:
+                                            snapshot.data!.active.toString()),
+                                    ReusableRow(
+                                        title: 'Critical',
+                                        value:
+                                            snapshot.data!.critical.toString()),
+                                    ReusableRow(
+                                        title: 'Affected Countries',
+                                        value: snapshot.data!.affectedCountries
+                                            .toString()),
+                                    ReusableRow(
+                                        title: 'Tests',
+                                        value: snapshot.data!.tests.toString()),
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          const CountriesListView()));
-                            },
-                            child: Container(
-                              height: 50,
-                              decoration: BoxDecoration(
-                                  color: const Color(0xff1aa260),
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: const Center(
-                                child: Text('Track Countries'),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const CountriesListView()));
+                              },
+                              child: Container(
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    color: const Color(0xff1aa260),
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: const Center(
+                                  child: Text('Track Countries'),
+                                ),
                               ),
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  }
-                }),
-          ],
-        ),
-      )),
+                            )
+                          ],
+                        ),
+                      );
+                    }
+                  }),
+            ],
+          ),
+        )),
+      ),
     );
   }
 }
